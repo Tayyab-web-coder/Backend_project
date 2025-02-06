5a30564d98d421ebf06d3e7516dd595bfc574218
Tayyab-web-coder <tayyabirshad2103@gmail.com>
2025-02-06 23:02:12 +0500

    Custom Api Errors Handling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+import express, { urlencoded } from 'express'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config({
+    path: '/env'
+})
+const app = express()
+app.use(cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true
+}))
+
+app.use(express.json({ limit: '16kb' }))
+app.use(urlencoded({ limit: "16kb" }))
+app.use(express.static('public'))
+app.use(cookieParser())
+export default app
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import { Connection } from "./db/db.js";
-Connection()
+import dotenv from "dotenv"
+import  app  from "./app.js";
+dotenv.config({
+    path: './env'
+})
 
+
+Connection()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server Listen at : ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(`Error come from db: ${error}`);
+
+    })
